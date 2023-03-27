dc8de1347c261c252b63c4143513b575f42753a3
Q-Wednesday <1403292286@qq.com>
2023-03-27 16:28:47 +0800

    more test cases with different packet size

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func testUDPSender() {
 }
 
 func main() {
-	s := sender.NewPacketTrainServer(sender.PacketTrainSenderOption{PacketSize: 2048})
+	s := sender.NewPacketTrainServer(sender.PacketTrainSenderOption{PacketSize: 4096})
 	s.Listen(&net.TCPAddr{
 		IP:   net.IPv4(0, 0, 0, 0),
 		Port: 9878,
